4aed1c809a4a40eb5e35f94397b713326cd77cb7
rennanguedesroch-art <rennanguedesroch@gmail.com>
2026-03-07 23:27:12 -0300

    Create topico02.php

---
+++ topico02.php
@@ -0,0 +1,33 @@
+<?php
+echo "<h1>Tópico 02 </h1>";//explicação
+echo "Texto";#comentário
+print"<br>Outra linha<br>";
+
+/*
+comentario
+com varias
+linhas
+*/
+
+echo phpversion();
+echo "<hr>";
+
+$idade=25;//inteiro
+$nota=8.5;//float
+$nome="Bete";//string
+$nome1="Beto";
+$nome2="Roberta";
+
+$nota=10;
+//$nota=7;
+$nota="Errado!";
+
+echo $nota;
+echo "<br>";
+echo $nome;
+
+echo "<hr>";
+
+echo "$nome você tem $idade anos e <br>sua nota foi $nota";
+
+?>
